5d55bd93af8d1dfabc43c4498716261d977761bc
Oksana Belenkaya <aksanabelenkaya@yandex.ru>
2024-08-21 17:04:00 +0300

    KKK

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        int number = 9;
-        int result = FactorialCalculator.calculateFactorial(number);
-        System.out.println(number + "! = " + result + ";");
-    }
-}
